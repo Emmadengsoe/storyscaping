884f80fdadfa500d705fa14775c73fe39fa6d1a5
emmadengsoe <emmadengso@hotmail.com>
2020-11-13 10:47:28 +0100

    Update script.js

--- ask-a-question-plugin/js/script.js
+++ ask-a-question-plugin/js/script.js
@@ -1,9 +1,11 @@
+/*dette er til at man kan lukke pop-uppen ned*/
 jQuery(document).ready(function( $ ) {
   $('.popupCloseButton').on('click', function() {
     $('.login-form').hide();
   })
 });
 
+/*dette er til send knappen*/
 jQuery(document).ready(function( $ ) {
   $('#submitBtn').on('click', function() {
     $('.login-form').hide();
